9ac74bbcec504b01851d84de720c78b959bfb8c5
Alireza <jafari.dev@gmail.com>
2024-10-20 22:34:28 +0330

    add data sources

---
+++ app/src/main/java/com/example/mvvmtest/data/repository/movie/datasource/MovieCacheDataSource.kt
@@ -0,0 +1,10 @@
+package com.example.mvvmtest.data.repository.movie.datasource
+
+import com.example.mvvmtest.data.model.movie.Movie
+
+
+interface MovieCacheDataSource {
+    suspend fun getMoviesFromCache():List<Movie>
+    suspend fun saveMoviesToCache(movies:List<Movie>)
+
+}
